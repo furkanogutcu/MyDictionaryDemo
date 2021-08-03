73367f862f8221f223fa1a37e40ab10531b9487a
Furkan <Furkan>
2021-08-03 22:11:45 +0300

    MyDictinory

--- MyDictionaryDemo/Program.cs
+++ MyDictionaryDemo/Program.cs
@@ -7,14 +7,6 @@ namespace MyDictionaryDemo
     {
         static void Main(string[] args)
         {
-            Dictionary<int, string> dict1 = new Dictionary<int, string>();
-            dict1.Add(123, "Deneme");
-            dict1.Add(456, "Deneme2");
-            foreach(int key in dict1.Keys)
-            {
-                Console.WriteLine(key);
-            }
-
             MyDictionary<int, string> myDict = new MyDictionary<int, string>();
             myDict.Add(123, "Deneme");
             myDict.Add(456, "Deneme2");
